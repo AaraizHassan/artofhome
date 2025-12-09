8394af9922ac9ba51900068c16ed9285b615dfe0
AaraizHassan <23100300@lums.edu.pk>
2025-12-09 21:05:21 +0500

    page.tsx updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import FAQ from "@/components/FAQ";
 import Logos from "@/components/Logos";
 import Benefits from "@/components/Benefits/Benefits";
 import Container from "@/components/Container";
-import Section from "@/components/Section";
+// import Section from "@/components/Section";
 // import Stats from "@/components/Stats";
 // import CTA from "@/components/CTA";
 
