e58c416b7d6fc806fce36903cccc16428a9b2bc6
AaraizHassan <23100300@lums.edu.pk>
2025-12-12 18:47:33 +0500

    Client heading

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -27,7 +27,7 @@ const Clients: React.FC = () => {
   return (
     <section id="clients" className="py-10 lg:py-20">
       <h2 className="text-3xl font-bold text-center mb-10">
-        How Our Clients Display Their Art
+        Client Homes
       </h2>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-6">
